820da55cded69309446111ea70f0221d3f002486
Vladone9 <102283225+Vladone9@users.noreply.github.com>
2022-03-24 13:24:17 +0500

    Update arithmetic_encode.cpp

--- arithmetic_encode.cpp
+++ arithmetic_encode.cpp
@@ -12,9 +12,9 @@ class Grani
 {
 public:
 	char c;             // символ
-	unsigned int key;   // ÷àñòîòíîñòü
-	int left;           // ëåâàÿ ãðàíèöà
-	int right;          // ïðàâàÿ ãðàíèöà
+	unsigned int key;   // частотность
+	int left;           // левая граница
+	int right;          // правая граница
 };
 
 class MapKeys
@@ -24,24 +24,24 @@ public:
 
 	void CreateMap(ifstream& f)
 	{
-		// ñîçäàåì êîíòåéíåð map
-		// ñèìâîë ïðåäñòàâëÿåòñÿ êàê èíäåêñ
-		// è ïî ýòîìó èíäåêñó çíà÷åíèå ÿ÷åéêè óâåëè÷èâàåòñÿ íà 1
-		// òî åñòü èùåì êîë-âî âõîæäåíèé ñèìâîëà â òåêñò (÷àñòîòíîñòü)
+		// создаем контейнер map
+		// символ представляется как индекс
+		// и по этому индексу значение ячейки увеличивается на 1
+		// то есть ищем кол-во вхождений символа в текст (частотность)
 		for (char c = f.get(); c != EOF; c = f.get())
 		{
 			mp[c]++;
 		}
 		f.clear();
-		f.seekg(0);   // âîçâðàùàåì óêàçàòåëü â íà÷àëî ôàéëà
+		f.seekg(0);   // возвращаем указатель в начало файла
 	}
 
 	void PrintMap()
 	{
-		// begin() è end() ýòî íà÷àëî è êîíåö êîíòåéíåðà map
-		// ïðîõîäèìñÿ èòåðàòîðîì ïî êîíòåéíåðó è ïå÷àòàåì ñîäåðæèìîå
-		// it->first - ïå÷àòàåì ñèìâîë char
-		// it->second - ÷àñòîòíîñòü ñèìâîëà
+		// begin() и end() это начало и конец контейнера map
+		// проходимся итератором по контейнеру и печатаем содержимое
+		// it->first - печатаем символ char
+		// it->second - частотность символа
 		map<char, int>::iterator it;
 		for (it = mp.begin(); it != mp.end(); it++)
 		{
@@ -67,8 +67,8 @@ bool compare(Grani obj1, Grani obj2)
 
 int main()
 {
-	ifstream fin("text.txt", ios::binary);      // èñõîäíûé òåêñò
-	ofstream fout("encode.txt", ios::binary);   // ðåçóëüòàò êîäèðîâàíèÿ
+	ifstream fin("text.txt", ios::binary);      // исходный текст
+	ofstream fout("encode.txt", ios::binary);   // результат кодирования
 
 	MapKeys maap;
 	maap.CreateMap(fin);
@@ -82,20 +82,20 @@ int main()
 
 
 
-	// ïåðåïèñûâàåì ñîäåðæèìîå êîíòåéíåðà map â ñïèñîê
+	// переписываем содержимое контейнера map в список
 	for (itr = maap.mp.begin(); itr != maap.mp.end(); itr++)
 	{
 		Grani tmp;
-		tmp.c = itr->first;      // it->first - ñèìâîë char
-		tmp.key = itr->second;   // it->second - ÷àñòîòíîñòü
+		tmp.c = itr->first;      // it->first - символ char
+		tmp.key = itr->second;   // it->second - частотность
 		count += tmp.key;
-		spisok.push_back(tmp);   // ïîìåùàåì â êîíåö ñïèñêà
+		spisok.push_back(tmp);   // помещаем в конец списка
 	}
-	spisok.sort(compare);        // ñîðòèðóåì ñïèñîê (ïî óáûâàíèþ)
+	spisok.sort(compare);        // сортируем список (по убыванию)
 
 
 
-	// âûâîäèì ñîäåðæèìîå ñïèñêà
+	// выводим содержимое списка
 	cout << "spisok:" << endl;
 	int temp = 0;
 	for (auto i = spisok.begin(); i != spisok.end(); i++)
@@ -114,25 +114,25 @@ int main()
 
 
 
-	// âûïèñûâàåì êîíòåéíåð â ôàéë
+	// выписываем контейнер в файл
 	int k = maap.mp.size();
-	fout.write((char*)&k, sizeof(k));   // çàïèñûâàåì êîëè÷åñòâî ñèìâîëîâ ýëåìåíòîâ
+	fout.write((char*)&k, sizeof(k));   // записываем количество символов элементов
 	for (itr = maap.mp.begin(); itr != maap.mp.end(); itr++)
 	{
-		fout.write((char*)&itr->first, sizeof(itr->first));     // ïèøåì ñèìâîë
-		fout.write((char*)&itr->second, sizeof(itr->second));   // ïèøåì êîë-âî
+		fout.write((char*)&itr->first, sizeof(itr->first));     // пишем символ
+		fout.write((char*)&itr->second, sizeof(itr->second));   // пишем кол-во
 	}
 
 
 
 	list<Grani>::iterator gr;
-	unsigned short l = 0;      // íèæíÿÿ ãðàíèöà
-	unsigned short h = 65535;  // âåðõíÿÿ ãðàíèöà
-	int delitel = spisok.back().right;   // back() - óêàçàòåëü íà ïîñëåäíèé ýëåìåíò
+	unsigned short l = 0;      // нижняя граница
+	unsigned short h = 65535;  // верхняя граница
+	int delitel = spisok.back().right;   // back() - указатель на последний элемент
 	unsigned short first = (h + 1) / 4;   //16384
-	unsigned short half = first * 2;	  //32768
+	unsigned short half = first * 2;      //32768
 	unsigned short thride = first * 3;    //49152
-	int bitsf = 0;   // ñêîëüêî áèòîâ ñáðàñûâàòü
+	int bitsf = 0;   // сколько битов сбрасывать
 	char tmp = 0;
 	int ncount = 0;
 
@@ -145,8 +145,8 @@ int main()
 		}
 		for (gr = spisok.begin(); (gr != spisok.end()) && (gr->c != c); gr++);
 		int d = h - l + 1;
-		h = l + gr->right * d / delitel - 1;  // âåðõíÿÿ ãðàíèöà
-		l = l + gr->left * d / delitel;       // íèæíÿÿ ãðàíèöà
+		h = l + gr->right * d / delitel - 1;  // верхняя граница
+		l = l + gr->left * d / delitel;       // нижняя граница
 
 		while (1)
 		{
