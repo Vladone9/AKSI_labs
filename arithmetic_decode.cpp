2f3bcb94cc8d40d79a75a94269bc8d42004c0315
Vladone9 <102283225+Vladone9@users.noreply.github.com>
2022-03-24 13:19:09 +0500

    Update arithmetic_decode.cpp

--- arithmetic_decode.cpp
+++ arithmetic_decode.cpp
@@ -11,10 +11,10 @@ using namespace std;
 class Grani
 {
 public:
-	char c;             // символ
-	unsigned int key;   // частотность
-	int left;           // левая граница
-	int right;          // правая граница
+	char c;             // СЃРёРјРІРѕР»
+	unsigned int key;   // С‡Р°СЃС‚РѕС‚РЅРѕСЃС‚СЊ
+	int left;           // Р»РµРІР°СЏ РіСЂР°РЅРёС†Р°
+	int right;          // РїСЂР°РІР°СЏ РіСЂР°РЅРёС†Р°
 };
 
 class MapKeys
@@ -24,30 +24,30 @@ public:
 
 	void CreateMap(ifstream& f)
 	{
-		// создаем контейнер map
-		// символ представляется как индекс
-		// и по этому индексу значение ячейки увеличивается на 1
-		// то есть ищем кол-во вхождений символа в текст (частотность)
+		// СЃРѕР·РґР°РµРј РєРѕРЅС‚РµР№РЅРµСЂ map
+		// СЃРёРјРІРѕР» РїСЂРµРґСЃС‚Р°РІР»СЏРµС‚СЃСЏ РєР°Рє РёРЅРґРµРєСЃ
+		// Рё РїРѕ СЌС‚РѕРјСѓ РёРЅРґРµРєСЃСѓ Р·РЅР°С‡РµРЅРёРµ СЏС‡РµР№РєРё СѓРІРµР»РёС‡РёРІР°РµС‚СЃСЏ РЅР° 1
+		// С‚Рѕ РµСЃС‚СЊ РёС‰РµРј РєРѕР»-РІРѕ РІС…РѕР¶РґРµРЅРёР№ СЃРёРјРІРѕР»Р° РІ С‚РµРєСЃС‚ (С‡Р°СЃС‚РѕС‚РЅРѕСЃС‚СЊ)
 		for (char c = f.get(); c != EOF; c = f.get())
 		{
 			mp[c]++;
 		}
 		f.clear();
-		f.seekg(0);   // возвращаем указатель в начало файла
+		f.seekg(0);   // РІРѕР·РІСЂР°С‰Р°РµРј СѓРєР°Р·Р°С‚РµР»СЊ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№Р»Р°
 	}
 
 	int HeadRead(ifstream& f)
 	{
-		unsigned long long len = 0;   // длина общего текста, не считая головы
+		unsigned long long len = 0;   // РґР»РёРЅР° РѕР±С‰РµРіРѕ С‚РµРєСЃС‚Р°, РЅРµ СЃС‡РёС‚Р°СЏ РіРѕР»РѕРІС‹
 		int key;
 		int q;
 		char s;
 
 		f.read((char*)&q, sizeof(q));
 		for (int i = 0; i < q; i++) {
-			f.read((char*)&s, sizeof(s));		// считываем символ
-			f.read((char*)&key, sizeof(key));	// считываем его значение
-			mp[s] = key;						// помещаем ключ в контейнер map
+			f.read((char*)&s, sizeof(s));		// СЃС‡РёС‚С‹РІР°РµРј СЃРёРјРІРѕР»
+			f.read((char*)&key, sizeof(key));	// СЃС‡РёС‚С‹РІР°РµРј РµРіРѕ Р·РЅР°С‡РµРЅРёРµ
+			mp[s] = key;				// РїРѕРјРµС‰Р°РµРј РєР»СЋС‡ РІ РєРѕРЅС‚РµР№РЅРµСЂ map
 			len = len + key;
 		}
 
@@ -56,10 +56,10 @@ public:
 
 	void PrintMap()
 	{
-		// begin() и end() это начало и конец контейнера map
-		// проходимся итератором по контейнеру и печатаем содержимое
-		// it->first - печатаем символ char
-		// it->second - частотность символа
+		// begin() Рё end() СЌС‚Рѕ РЅР°С‡Р°Р»Рѕ Рё РєРѕРЅРµС† РєРѕРЅС‚РµР№РЅРµСЂР° map
+		// РїСЂРѕС…РѕРґРёРјСЃСЏ РёС‚РµСЂР°С‚РѕСЂРѕРј РїРѕ РєРѕРЅС‚РµР№РЅРµСЂСѓ Рё РїРµС‡Р°С‚Р°РµРј СЃРѕРґРµСЂР¶РёРјРѕРµ
+		// it->first - РїРµС‡Р°С‚Р°РµРј СЃРёРјРІРѕР» char
+		// it->second - С‡Р°СЃС‚РѕС‚РЅРѕСЃС‚СЊ СЃРёРјРІРѕР»Р°
 		map<char, int>::iterator it;
 		for (it = mp.begin(); it != mp.end(); it++)
 		{
@@ -89,7 +89,7 @@ int main()
 	ofstream fout("decode.txt", ios::binary);
 
 	MapKeys maap;
-	// считываем контейнер из файла и получаем общую длину
+	// СЃС‡РёС‚С‹РІР°РµРј РєРѕРЅС‚РµР№РЅРµСЂ РёР· С„Р°Р№Р»Р° Рё РїРѕР»СѓС‡Р°РµРј РѕР±С‰СѓСЋ РґР»РёРЅСѓ
 	int len = maap.HeadRead(fin);
 	cout << "PrintMap:" << endl;
 	maap.PrintMap();
@@ -101,20 +101,20 @@ int main()
 
 
 
-	// переписываем содержимое контейнера map в список
+	// РїРµСЂРµРїРёСЃС‹РІР°РµРј СЃРѕРґРµСЂР¶РёРјРѕРµ РєРѕРЅС‚РµР№РЅРµСЂР° map РІ СЃРїРёСЃРѕРє
 	for (itr = maap.mp.begin(); itr != maap.mp.end(); itr++)
 	{
 		Grani tmp;
-		tmp.c = itr->first;      // it->first - символ char
-		tmp.key = itr->second;   // it->second - частотность
+		tmp.c = itr->first;      // it->first - СЃРёРјРІРѕР» char
+		tmp.key = itr->second;   // it->second - С‡Р°СЃС‚РѕС‚РЅРѕСЃС‚СЊ
 		count += tmp.key;
-		spisok.push_back(tmp);   // помещаем в конец списка
+		spisok.push_back(tmp);   // РїРѕРјРµС‰Р°РµРј РІ РєРѕРЅРµС† СЃРїРёСЃРєР°
 	}
-	spisok.sort(compare);        // сортируем список (по убыванию)
+	spisok.sort(compare);        // СЃРѕСЂС‚РёСЂСѓРµРј СЃРїРёСЃРѕРє (РїРѕ СѓР±С‹РІР°РЅРёСЋ)
 
 
 
-	// выводим содержимое списка
+	// РІС‹РІРѕРґРёРј СЃРѕРґРµСЂР¶РёРјРѕРµ СЃРїРёСЃРєР°
 	cout << "spisok:" << endl;
 	int temp = 0;
 	for (auto i = spisok.begin(); i != spisok.end(); i++)
@@ -134,11 +134,11 @@ int main()
 
 
 	list<Grani>::iterator gr;
-	unsigned short l = 0;      // нижняя граница
-	unsigned short h = 65535;  // верхняя граница
-	int last = spisok.back().right;   // back() - указатель на последний элемент
+	unsigned short l = 0;      // РЅРёР¶РЅСЏСЏ РіСЂР°РЅРёС†Р°
+	unsigned short h = 65535;  // РІРµСЂС…РЅСЏСЏ РіСЂР°РЅРёС†Р°
+	int last = spisok.back().right;   // back() - СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РїРѕСЃР»РµРґРЅРёР№ СЌР»РµРјРµРЅС‚
 	unsigned short first = (h + 1) / 4;   //16384
-	unsigned short half = first * 2;	  //32768
+	unsigned short half = first * 2;      //32768
 	unsigned short thride = first * 3;    //49152
 	int value = (fin.get() << 8) | fin.get();
 	char tmp = fin.get();
@@ -149,8 +149,8 @@ int main()
 		int d = h - l + 1;
 		unsigned int freq = (((value - l + 1) * last) - 1) / d;
 		for (gr = spisok.begin(); gr->right <= freq; gr++);
-		h = l + gr->right * d / last - 1;  // верхняя граница
-		l = l + gr->left * d / last;       // нижняя граница
+		h = l + gr->right * d / last - 1;  // РІРµСЂС…РЅСЏСЏ РіСЂР°РЅРёС†Р°
+		l = l + gr->left * d / last;       // РЅРёР¶РЅСЏСЏ РіСЂР°РЅРёС†Р°
 		
 		while (1)
 		{
